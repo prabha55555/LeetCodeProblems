4db1a2460fbe061b722316427e0d48c8832e815f
Prabhakaran K <125977684+prabha55555@users.noreply.github.com>
2025-05-25 23:08:07 +0530

    Time: 8 ms (24.27%), Space: 45.6 MB (16.23%) - LeetHub

---
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minPartitions(String n) {
+
+        char[]arr = n.toCharArray();
+
+        Arrays.sort(arr);
+
+        return arr[arr.length - 1] - '0';
+
+
+    }
+}
