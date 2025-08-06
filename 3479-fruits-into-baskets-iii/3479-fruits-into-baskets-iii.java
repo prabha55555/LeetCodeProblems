691599ae05081f77f99dab9eaa2cd4cf8bbd1e80
Prabhakaran K <125977684+prabha55555@users.noreply.github.com>
2025-08-06 17:48:03 +0530

    Time: 31 ms (100%), Space: 57.5 MB (91.89%) - LeetHub

---
+++ 3479-fruits-into-baskets-iii/3479-fruits-into-baskets-iii.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        // int count=0,n=fruits.length;
+
+        
+
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(fruits[i]<=baskets[j]){
+        //             count++;
+        //             baskets[j]=-1;
+        //             break;                     Time Limit Exceed
+        //         }
+        //     }
+        // }
+
+        // return n-count;
+
+     
+        int n = baskets.length;
+        int N = 1;
+        while(N <= n) N <<= 1;
+        // Build the segment tree
+        int[] segTree = new int[N << 1];
+        for (int i = 0; i < n; i++)
+        segTree[N + i] = baskets[i];
+        for (int i = N - 1; i > 0; i--) 
+        segTree[i] = Math.max(segTree[2 * i], segTree[2 * i + 1]);
+        int count = 0;
+        for (int i = 0; i < n; ++i) {
+            int x = fruits[i];
+            int index = 1; // Start from the root of the segment tree
+            if (segTree[index] < x) {
+                count++;
+                continue;
+            }
+            // Query the first valid basket
+            while (index < N) {
+                if (segTree[index * 2] >= x) 
+                index *= 2;
+                else
+                index = index * 2 + 1;
+            }
+            // Mark the found basket as used and update the segment tree
+            segTree[index] = -1;
+            while (index > 1) {
+                index >>= 1;
+                segTree[index] = Math.max(segTree[2 * index], segTree[2 * index + 1]);
+            }
+        }
+        return count;     
+    
+
+    }
+}
