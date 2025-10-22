18132b7f68eb27bc8d42895e8d34aaeab397f719
Prabhakaran K <125977684+prabha55555@users.noreply.github.com>
2025-10-22 22:03:10 +0530

    Time: 0 ms (100%), Space: 40.8 MB (56.79%) - LeetHub

--- 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.java
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.java
@@ -4,14 +4,11 @@ class Solution {
         int sum1=0,sum2=0;
 
         for(int i=1;i<=n;i++){
-            if(i % m ==0){
-                sum2+=i;
-            }
-            else{
-                sum1+=i;
-            }
+            if(i%m==0)sum1+=i;
+            else sum2+=i;
         }
-        return sum1-sum2;
+
+        return sum2 -sum1;
         
     }
 }
