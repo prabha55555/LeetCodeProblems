ff151010949552e31855e7e5346f4d25c2353618
Prabhakaran K <125977684+prabha55555@users.noreply.github.com>
2026-09-21 15:06:28 +0530

    Time: 1 ms (100%), Space: 44.2 MB (34.14%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -1,24 +1,33 @@
 class Solution {
     public int reverseDegree(String s) {
 
-        HashMap<Character,Integer> values = new HashMap<>();
-        int num=26;
+        // HashMap<Character,Integer> values = new HashMap<>();
+        // int num=26;
 
-        for(char val = 'a' ; val<='z';val++){
+        // for(char val = 'a' ; val<='z';val++){
 
-            values.put(val,num);
-            num--;
+        //     values.put(val,num);
+        //     num--;
 
-        }
+        // }
 
-        int sum=0;
+        // int sum=0;
 
-        for(int i=0;i<s.length();i++){
+        // for(int i=0;i<s.length();i++){
 
-            sum+=values.get(s.charAt(i)) * (i+1);
-        }
+        //     sum+=values.get(s.charAt(i)) * (i+1);
+        // }
 
 
-        return sum;
+        // return sum;
+
+
+         int sum = 0;
+      for(int i = 1;i<=s.length();i++){
+            char ch =  s.charAt(i-1);
+            int num = 'z' -ch +1;
+            sum += i*num;
+      }
+      return sum;
     }
 }
